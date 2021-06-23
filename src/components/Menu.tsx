5889b4d0704390860ec64227892618f23eb5a398
Dome C <dome@tel.co.th>
2021-06-23 22:31:12 +0700

    fix menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,31 +9,12 @@ import { I18n } from '@lingui/core'
 import { useLingui } from '@lingui/react'
 
 const items = (i18n: I18n) => [
-    {
-        name: i18n._(t`Docs`),
-        description: i18n._(t`Documentation for users of Sushi.`),
-        href: 'https://docs.sushi.com'
-    },
-    {
-        name: i18n._(t`Dev`),
-        description: i18n._(t`Documentation for developers of Sushi.`),
-        href: 'https://dev.sushi.com'
-    },
+
     {
         name: i18n._(t`Open Source`),
-        description: i18n._(t`Sushi is a supporter of Open Source.`),
-        href: 'https://github.com/sushiswap'
+        description: i18n._(t`BOLO is a supporter of Open Source.`),
+        href: 'https://github.com/bolo2'
     },
-    {
-        name: i18n._(t`Tools`),
-        description: i18n._(t`Tools to optimize your workflow.`),
-        href: '/tools'
-    },
-    {
-        name: i18n._(t`Discord`),
-        description: i18n._(t`Join the community on Discord.`),
-        href: 'https://discord.gg/NVPXN4e'
-    }
 ]
 
 export default function Menu() {
